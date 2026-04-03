6b77149c928265c8492fb23b1b6b6bf08f2c5bd0
Myst-12 <kushal11.mittal@gmail.com>
2026-04-03 10:38:04 +0530

    fixed api problem

--- src/components/QuotesList.jsx
+++ src/components/QuotesList.jsx
@@ -6,26 +6,15 @@ export default function QuotesList() {
     const [quotes, setQuotes] = useState([]);
 
     useEffect(() => {
-        fetch("https://api.allorigins.win/get?url=https://zenquotes.io/api/quotes")
-            .then(res => res.json())
-            .then(data => {
-                console.log("RAW:", data);
-
-                if (!data.contents) {
-                    console.log("No contents received");
-                    return;
-                }
-
-                try {
-                    const parsed = JSON.parse(data.contents);
-                    console.log("PARSED:", parsed);
-                    setQuotes(parsed);
-                } catch (e) {
-                    console.log("JSON parse error", e);
-                }
-            })
-            .catch(err => console.log(err));
-    }, []);
+    fetch("https://corsproxy.io/?https://zenquotes.io/api/quotes")
+        .then(res => res.text())
+        .then(data => {
+            const parsed = JSON.parse(data);
+            console.log(parsed);
+            setQuotes(parsed);
+        })
+        .catch(err => console.log(err));
+}, []);
 
     return (
         <div>
